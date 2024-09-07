7651a72ec9be15adc883593ab8ba7ec55f531001
Kumoichi <minimi_ken1@yahoo.co.jp>
2024-09-07 17:52:20 +0900

    initial condition

--- src/App.js
+++ src/App.js
@@ -1,16 +1,4 @@
-import Header from './Header.js'
-import Footer from './Footer.js'
-
 function App() {
-  return(
-    <>
-      <Header/>
-      <Footer/>
-    </>
-  );
 }
 
 export default App;
-
-
-//new branch test
