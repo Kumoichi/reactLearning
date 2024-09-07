387a435868a0a9a81d81ef1c4594c20de6594a35
Kumoichi <minimi_ken1@yahoo.co.jp>
2024-09-07 16:55:52 +0900

    food component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './Header.js'
 import Footer from './Footer.js'
+import Food from './Food.js'
 
 function App() {
   return(
     <>
       <Header/>
+      <Food/>
       <Footer/>
     </>
   );
