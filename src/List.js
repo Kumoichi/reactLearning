d4661d3004d27bb07709dd2e26713c200fb9bd6c
Kumoichi <minimi_ken1@yahoo.co.jp>
2024-09-16 00:36:16 +0900

    props types addition

--- src/List.js
+++ src/List.js
@@ -4,6 +4,7 @@
 //id, name, caloriesをリストの中に入れる
 //fruitsのcaloriesが100以下のlistを取り出す
 //一つ一つのlistをプリントする
+import PropTypes from 'prop-types';
 
 function List(props) {
     const category = props.category;
@@ -15,5 +16,15 @@ function List(props) {
     </li>)
     return(<><h3 className="list-category">{category}</h3>
     <ol className="list-items">{listItems}</ol></>);
+}
+List.propTypes = {
+    category: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.shape({id: PropTypes.number,
+                                              name: PropTypes.string,
+                                            calories: PropTypes.number})),
+}
+List.defaultProps = {
+    category: "Category",
+    item: [],
 }
 export default List
