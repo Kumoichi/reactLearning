44b43b5ab06b571b3f5415d06f75813dbe596e27
Kumoichi <minimi_ken1@yahoo.co.jp>
2024-09-23 10:20:49 +0900

    setname and age with useState

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,18 +1,30 @@
 import React, {useState} from 'react';
 
 function MyComponent() {
-    let [name, setName] = useState();
+    const [name, setName] = useState("Guest");
+    const [age, setAge] = useState(0);
 
     const updateName = () => {
-        name = "Spongebob";
-        console.log(name);
+        setName("Spongebob")
+    }
+
+    const incrementAge = () => {
+        setAge(age + 1);
     }
     return(<div>
         <p>Name: {name}</p>
         <button onClick={updateName}>Set Name</button>
+
+        <p>Age: {age}</p>
+        <button onClick={incrementAge}>increment Age</button>
     </div>)
 }
 
 export default MyComponent
 
-//test pushing
+
+
+// useState解説
+// 左は値、右は関数って感じかな
+
+// useState("Guest")これはinitial valueがGuestになる
