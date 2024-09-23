1ac294e580a8f755e545cc93eadcb6047e087218
Kumoichi <minimi_ken1@yahoo.co.jp>
2024-09-23 10:06:12 +0900

    sename on console

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,5 +1,16 @@
-function MyComponent() {
+import React, {useState} from 'react';
 
+function MyComponent() {
+    let [name, setName] = useState();
+
+    const updateName = () => {
+        name = "Spongebob";
+        console.log(name);
+    }
+    return(<div>
+        <p>Name: {name}</p>
+        <button onClick={updateName}>Set Name</button>
+    </div>)
 }
 
 export default MyComponent
