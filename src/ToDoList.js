d337b52bc388e317cc712c1bd506a1e394adfb8d
Kumoichi <minimi_ken1@yahoo.co.jp>
2024-10-03 00:27:40 +0900

    usestate and movement control

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -10,19 +10,33 @@ const ToDoList = () => {
     }
 
     function addTask(){
-
+        if(newTask.trim() !== ""){
+            setTasks(t => [...t, newTask]);
+            setNewTask("");
+        }
     }
 
     function deleteTask(index){
-
+        const updatedTasks = tasks.filter((_, i) => i !== index);
+        setTasks(updatedTasks);
     }
 
     function moveTaskUp(index){
-
+        if(index > 0) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index],updatedTasks[index - 1]] = 
+            [updatedTasks[index - 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
     }
 
     function moveTaskDown(index){
-
+        if(index < tasks.length - 1) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index],updatedTasks[index + 1]] = 
+            [updatedTasks[index + 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
     }
     
   return (
@@ -51,6 +65,11 @@ const ToDoList = () => {
                     className="move-button"
                     onClick={() => moveTaskUp(index)}>
                         UP
+                    </button>
+                    <button
+                    className="move-button"
+                    onClick={() => moveTaskDown(index)}>
+                        down
                     </button>
                 </li>)}
             </ol>
